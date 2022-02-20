a7cccc0b81ad45c381cf061394d8c6456f65eb3a
Lishan1212 <lishalishan12.com>
2022-02-20 10:36:23 +0530

    live location added

---
+++ live_location_tracker/lib/main.dart
@@ -0,0 +1,137 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, unused_import, non_constant_identifier_names, prefer_const_constructors, duplicate_ignore, unused_field, unused_element, avoid_print
+
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:live_location_tracker/mymap.dart';
+import 'package:location/location.dart' as loc;
+import 'package:permission_handler/permission_handler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(home: MyApp()));
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  final loc.Location location = loc.Location();
+  StreamSubscription<loc.LocationData>? _locationSubscription;
+
+  @override
+  void initState(){
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+    
+      appBar: AppBar(
+    
+        title:Text('live location tracker')
+        
+      ),
+      body: Column(children: [
+        // ignore: prefer_const_constructors
+        TextButton(onPressed:() {} ,child:Text('add my location')),
+        TextButton(onPressed:() {} ,child:Text('Enable live location')),
+        TextButton(onPressed:() {} ,child:Text('Stop live location')),
+        Expanded(child: StreamBuilder(stream: FirebaseFirestore.instance.collection('location').snapshots(),
+        builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
+          if(!snapshot.hasData){
+            return Center(child: CircularProgressIndicator());
+        }
+        return ListView.builder(
+          itemCount: snapshot.data?.docs.length,
+          itemBuilder:(context,index)
+          {
+            return ListTile(
+                title:
+                  Text(snapshot.data!.docs[index]['name'].toString()),
+                  subtitle: Row(
+                  children: [
+                          Text(snapshot.data!.docs[index]['latitude']
+                              .toString()),
+                          SizedBox(
+                            width: 20,
+                          ),
+                   Text(snapshot.data!.docs[index]['longitude']
+                              .toString()),
+                        ],
+                      ),
+                      //Text
+                 trailing: IconButton(
+                        icon: Icon(Icons.directions),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  MyMap(snapshot.data!.docs[index].id)));
+                        },
+                      ),
+                    );
+                  });
+            },
+          )),
+        ],
+      ),
+    );
+  }
+
+  _getLocation() async {
+    try {
+      final loc.LocationData _locationResult = await location.getLocation();
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': _locationResult.latitude,
+        'longitude': _locationResult.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
+  Future<void> _listenLocation() async {
+    _locationSubscription = location.onLocationChanged.handleError((onError) {
+      // ignore: avoid_print
+      print(onError);
+      _locationSubscription?.cancel();
+      setState(() {
+        _locationSubscription = null;
+      });
+    }).listen((loc.LocationData currentlocation) async {
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': currentlocation.latitude,
+        'longitude': currentlocation.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    });
+  }
+
+  // ignore: unused_element
+  _stopListening() {
+    _locationSubscription?.cancel();
+    setState(() {
+      _locationSubscription = null;
+    });
+  }
+
+  _requestPermission() async {
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      print('done');
+    } else if (status.isDenied) {
+      _requestPermission();
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings();
+    }
+  }
+}
